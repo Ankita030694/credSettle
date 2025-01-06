24b7240ec5bd59379de1f30f9c8ce70cddd94bdc
Ankita030694 <Ankitamalik03@gmail.com>
2025-01-06 03:36:36 -0800

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ComingSoon from './components/ComingSoon'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -6,16 +6,13 @@ import { faFacebook, faInstagram, faTwitter, faLinkedin } from '@fortawesome/fre
 const ComingSoon = () => {
   return (
     <div className="container">
-      {/* Background */}
-      <div className="background"></div>
-
-      {/* Middle content */}
-      <div className="content">
+      {/* Glassy Box */}
+      <div className="glassy-box">
         <h1>COMING SOON</h1>
         {/* Social Icons below "COMING SOON" */}
         <div className="social-icons">
+          <a href="https://www.instagram.com/credsettle/profilecard/?igsh=MWE0b24wa3AzcnVseA%3D%3D"><FontAwesomeIcon icon={faInstagram} size="2x" className="socials" /></a>
           <a href="#"><FontAwesomeIcon icon={faFacebook} size="2x" className="socials" /></a>
-          <a href="#"><FontAwesomeIcon icon={faInstagram} size="2x" className="socials" /></a>
           <a href="#"><FontAwesomeIcon icon={faTwitter} size="2x" className="socials" /></a>
           <a href="#"><FontAwesomeIcon icon={faLinkedin} size="2x" className="socials" /></a>
         </div>
