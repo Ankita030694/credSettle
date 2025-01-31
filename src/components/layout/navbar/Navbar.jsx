5ffe5cbbf8dc10c91f31718a3c71ead5453a93b6
Bhavya <Ankitamalik03@gmail.com>
2025-01-31 17:12:40 +0530

    changes

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
 import logo from "../../../assets/images/transparent.png";
 import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  const scrollToSection = (id) => {
-    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-  };
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
     <div className="bg-body-tertiary">
       <nav className="navbar navbar-expand-lg">
@@ -23,61 +23,62 @@ const Navbar = () => {
             <div className="col-4 text-center">
               <div className="collapse navbar-collapse">
                 <ul className="navbar-nav justify-content-center">
-                  <Link to="/" style={{textDecoration: "none"}}>
+                  <Link to="/" style={{ textDecoration: "none" }}>
                     <li className="nav-item links">
-                      <a
-                        className="nav-link active"
-                        aria-current="page"
-                        href="/"
-                        style={{textDecoration: "none"}}
-                      >
+                      <a className="nav-link active" href="/">
                         Home
                       </a>
                     </li>
                   </Link>
-                  <Link to="/about" style={{textDecoration: "none"}}>
+                  <Link to="/about" style={{ textDecoration: "none" }}>
                     <li className="nav-item links">
-                      <a
-                        className="nav-link"
-                        // onClick={() => scrollToSection("about")}
-                        style={{textDecoration: "none"}}
-                      >
-                        About Us
-                      </a>
+                      <a className="nav-link">About Us</a>
                     </li>
                   </Link>
-                  <Link to="/services" style={{textDecoration: "none"}}>
+                  
+                  {/* Services Dropdown */}
+                  <li 
+                    className="nav-item dropdown links"
+                    onMouseEnter={() => setDropdownOpen(true)}
+                    onMouseLeave={() => setDropdownOpen(false)}
+                  >
+                    <a className="nav-link dropdown-toggle" href="#">
+                      Services
+                    </a>
+                    {dropdownOpen && (
+                      <ul className="dropdown-menu show">
+                        <li>
+                          <Link className="dropdown-item" to="/services/personalloan">Personal Loan</Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="/services/creditloan">Credit Card</Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="/services/businessloan">Business Loan</Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="/services/carloan">Car Loan</Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="/services/antiharassement">Anti Harassement</Link>
+                        </li>
+                        <li>
+                          <Link className="dropdown-item" to="/services/creditscore">Credit Score Builder</Link>
+                        </li>
+                        
+                      </ul>
+                    )}
+                  </li>
+                  
+                  <Link to="/faq" style={{ textDecoration: "none" }}>
                     <li className="nav-item links">
-                      <a
-                        className="nav-link"
-                        onClick={() => scrollToSection("services")}
-                        style={{textDecoration: "none"}}
-                      >
-                        Services
-                      </a>
+                      <a className="nav-link">FAQ'S</a>
                     </li>
                   </Link>
-                  <Link to = "faq" style={{textDecoration: "none"}}>
-                  <li className="nav-item links">
-                    <a
-                      className="nav-link"
-                      onClick={() => scrollToSection("faq")}
-                      style={{textDecoration: "none"}}
-                    >
-                      FAQ'S
-                    </a>
-                  </li>
-                  </Link>
-                  <Link to = "testimonials" style={{textDecoration: "none"}}>
-                  <li className="nav-item links">
-                    <a
-                      className="nav-link"
-                      onClick={() => scrollToSection("testimonials")}
-                      
-                    >
-                      Blogs
-                    </a>
-                  </li>
+                  <Link to="/testimonials" style={{ textDecoration: "none" }}>
+                    <li className="nav-item links">
+                      <a className="nav-link">Blogs</a>
+                    </li>
                   </Link>
                 </ul>
               </div>
