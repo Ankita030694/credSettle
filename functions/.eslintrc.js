f1fb70a1b5e735e4c8a442dc2c156a0a30b4e950
bhavya <bhavyajain817@gmail.com>
2025-02-08 13:14:18 +0530

    changes

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,28 +1,22 @@
+/* eslint-env node */
 module.exports = {
+  root: true, // Ensures this is the base ESLint config
   env: {
+    node: true, // ✅ Fixes 'require' and 'module.exports' errors
     es6: true,
-    node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
+    ecmaVersion: 2018,
+    sourceType: "script", // ✅ Ensures ESLint treats files as CommonJS, not ES modules
   },
   extends: [
     "eslint:recommended",
     "google",
   ],
   rules: {
+    "no-undef": "off", // ✅ Allows 'require', 'exports', and 'module.exports'
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
   },
-  overrides: [
-    {
-      files: ["**/*.spec.*"],
-      env: {
-        mocha: true,
-      },
-      rules: {},
-    },
-  ],
-  globals: {},
 };
